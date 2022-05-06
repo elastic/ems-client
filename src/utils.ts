213b7c0504d8c7557bc1478f2f5a72b94270e36d
Jorge Sanz <jorge@jorgesanz.net>
2022-05-06 10:37:45 +0200

    Update src/utils.ts
    Co-authored-by: Nick Peihl <nickpeihl@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -58,15 +58,7 @@ export type layerPaintProperty = {
   'text-halo-color'?: DataDrivenPropertyValueSpecification<ColorSpecification>;
 };
 
-export type blendMode =
-  | 'multiply'
-  | 'darken'
-  | 'lighten'
-  | 'screen'
-  | 'overlay'
-  | 'burn'
-  | 'dodge'
-  | 'mix';
+export type blendMode = Parameters<ChromaStatic["blend"]>[2] | 'mix';
 
 /*
 Function to transform a maplibre color definition by a given function.
